f74803a43cd28738007939c8279f5e0d0639ac9c
Daniel Del Rio <ddelrio1986@gmail.com>
2016-01-26 11:56:17 -0500

    Add a bit of comments

--- bin/superheroes.php
+++ bin/superheroes.php
@@ -9,10 +9,13 @@ $options = getopt('a', ['all']);
 
 $superheroes = new Superheroes;
 
+// Print all superheroes.
 if (count($options)) {
     array_walk($superheroes->all, function ($superhero) {
         echo $superhero . "\n";
     });
+
+// Print one random superhero.
 } else {
     echo $superheroes->random() . "\n";
 }
