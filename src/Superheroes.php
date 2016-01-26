cb47cabc74b1aa587a39c2aca5a9834e71449251
Daniel Del Rio <ddelrio1986@gmail.com>
2016-01-26 10:54:52 -0500

    Load up superheroes

--- src/Superheroes.php
+++ src/Superheroes.php
@@ -1,8 +1,39 @@
 <?php
+/**
+ * Superheroes
+ *
+ * The main superheroes class.
+ *
+ * @package DDelrio1986\Superheroes
+ * @filesource
+ */
 
 namespace DDelrio1986\Superheroes;
 
+/**
+ * Superheroes
+ *
+ * The main superheroes class.
+ *
+ * @package DDelrio1986\Superheroes
+ */
 class Superheroes
 {
 
+    /**
+     * All
+     *
+     * @var array All of the superheroes.
+     */
+    public $all = array();
+
+    /**
+     * __construct
+     *
+     * Loads all of the superheroes.
+     */
+    public function __construct()
+    {
+        $this->all = require __DIR__ . '/../data/superheroes.php';
+    }
 }
--- tests/SuperheroesTest.php
+++ tests/SuperheroesTest.php
@@ -1,11 +1,46 @@
 <?php
+/**
+ * Superheroes Test
+ *
+ * Tests the superheroes class.
+ *
+ * @package DDelrio1986\SuperheroesTest
+ * @filesource
+ */
+
+namespace DDelrio1986\SuperheroesTest;
 
 use DDelrio1986\Superheroes\Superheroes;
+use PHPUnit_Framework_TestCase;
 
+/**
+ * Superheroes Test
+ *
+ * Tests the superheroes class.
+ *
+ * @package DDelrio1986\SuperheroesTest
+ */
 class SuperheroesTest extends PHPUnit_Framework_TestCase
 {
+
+    /**
+     * Test Superheroes Class Exists
+     *
+     * Tests if the superheroes class exists.
+     */
     public function testSuperheroesClassExists()
     {
         $this->assertEquals(true, class_exists('DDelrio1986\Superheroes\Superheroes'));
+    }
+
+    /**
+     * Test Names Loaded
+     *
+     * Tests that superhero names are loaded.
+     */
+    public function testNamesLoaded()
+    {
+        $superheroes = new Superheroes;
+        $this->assertGreaterThan(0, count($superheroes->all));
     }
 }
